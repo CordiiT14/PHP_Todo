7dde9bc69904356fa22560d39f0d4f65cf9d641d
CordiiT14 <cordelia.toennies@live.com>
2022-06-22 20:18:42 +0100

    added readme and started index layout

---
+++ README.md
@@ -0,0 +1,8 @@
+### PHP practice -> Todo list
+
+First PHP practise app - Todo List
+- Add tasks
+- Delete tasks
+- Mark as complete
+
+MySQL DB, styled using MaterializeCSS
--- html.php
+++ html.php
@@ -4,14 +4,63 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style> 
+        .primary {
+            background-color: #7BAE7F;
+        }
+        .secondary {
+            background-color: #F4EDEA;
+        }
+        h1 {
+        margin: 0;
+        padding: 5px;
+        font-size: 50px;
+        }
+        h4{
+        font-size: 20px;
+        }
+        body {
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+        }
+        main {
+        flex: 1 0 auto;
+        display: block;
+        margin: auto;
+        }
+        form{
+            max-width: 450px;
+        }
+    </style>
 </head>
 <body>
-    
+    <nav>
+        <div class="nav-wrapper primary">
+            <h1 class="center">Todo List</h1>
+        </div>
+    </nav>
+    <main>
+
+        <div class="card center z-depth-0 secondary" style="max-width: 300px; padding: 10px; margin: 15px;">
+            <form action="index.html" method="POST">
+                <div class="input-field">
+                    <input type="text" id="new_task" >
+                    <label for="new_task" class="active">Add new task:</label>
+
+                </div>
+                <input class="btn primary"type="submit" value="submit">
+            </form>
+        </div>
+
+    </main>
+    <footer class="page-footer primary">
+        <p class="center"> © 2022 Cordii.T Creative</p>
+    </footer>
 </body>
 </html>
